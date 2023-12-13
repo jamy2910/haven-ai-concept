36639f3f16d2745cb2f26f572015c770182859ca
jamy2910 <jamiecolley02@gmail.com>
2023-12-13 09:00:30 +0000

    Update vite.config.js

--- haven-ai/vite.config.js
+++ haven-ai/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Haven-concept",
+  base: "/haven-ai-concept",
   plugins: [react()],
 })
